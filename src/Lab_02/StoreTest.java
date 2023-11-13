5cf069b6e33e46e61111a4b1bda62367aa77304a
nhunggngn <ntnhunggl@gmail.com>
2023-11-13 17:57:51 +0700

    update

---
+++ src/Lab_02/StoreTest.java
@@ -0,0 +1,13 @@
+package Lab_02;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        Store store = new Store();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                "Animation", "Roger Allers", 87, 19.95f);
+        store.addDVD(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star wars",
+                "Science Fiction", "George Lucas", 87, 24.95f);
+        store.addDVD(dvd2);
+    }
+}
