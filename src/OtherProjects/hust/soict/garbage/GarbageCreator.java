dbd83be477a67b19fb536bfdc5982db6dc8ea910
nhunggngn <ntnhunggl@gmail.com>
2023-11-28 15:26:21 +0700

    update

--- src/OtherProjects/hust/soict/garbage/GarbageCreator.java
+++ src/OtherProjects/hust/soict/garbage/GarbageCreator.java
@@ -1,2 +1,29 @@
-package OtherProjects.hust.soict.garbage;public class GarbageCreator {
+package OtherProjects.hust.soict.garbage;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class GarbageCreator {
+    public void createGarbage(){
+        String filename = "test.exe";
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+
+        try {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        startTime = System.currentTimeMillis();
+        //Sử dụng StringBuffer
+        StringBuilder outputStringBuilder = new StringBuilder();
+        //String outputString = "";
+        for(byte b : inputBytes){
+            outputStringBuilder.append((char)b);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+    }
 }
